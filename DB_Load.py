7d4493add0b64fde3c84070b6c56ba6e271a62a9
Dhurai1309 <s.dhurairaj1991@gmail.com>
2025-06-19 14:19:06 +0530

    latest

---
+++ DB_Load.py
@@ -0,0 +1,11 @@
+import os
+import psycopg2
+import json
+import git
+import pandas as pd
+
+# mydb = psycopg2.connect(
+#     host="localhost",
+#     database="your_database",
+#     user="your_user",
+#     password="your_password"
